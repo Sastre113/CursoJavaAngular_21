8546bfb3f62acf864f4966e374cba584eae18ec3
Miguel <sastre113@gmail.com>
2021-02-05 13:49:42 +0100

    Preparado repositorio base.

---
+++ src/repositorioBase/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package repositorioBase;
+
+/**
+ * @author Miguel A. Sastre
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
