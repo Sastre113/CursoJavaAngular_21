1c0bf8c590ba8e50201c8bcc31f3ce6678766eff
Miguel <sastre113@gmail.com>
2021-02-06 02:34:15 +0100

    Ejercicio3 terminado.

--- src/default_Package/Ejercicio3.java
+++ src/default_Package/Ejercicio3.java
@@ -1,5 +1,8 @@
 package default_Package;
 
+import java.util.Hashtable;
+import java.util.Set;
+
 public class Ejercicio3 {
 	/*
 	 * 3) Crea una base de datos de 10 artículos para controlar el stock de
@@ -10,8 +13,61 @@ public class Ejercicio3 {
 	 * incluso listar toda la información en la terminal del programa
 	 */
 
-	public static void main() {
+	static Hashtable<String,Double> dBArticulos = new Hashtable<String,Double>(); 
+	
 
+	static private void mostrarStock() {
+		Set<String> it = dBArticulos.keySet();
+		String text = "";
+		for(String articulo : it) {
+			text += articulo + " " + dBArticulos.get(articulo).toString() + " €\n";
+		}
+		
+		AuxMethod.mostrarInfo(text);
 	}
-
+	
+	static private void cargarDB() {
+		dBArticulos.put("coca cola", 3.0);
+		dBArticulos.put("pepsi", 2.9);
+		dBArticulos.put("cerveza", 4.5);
+		dBArticulos.put("lejia", 6.6);
+		dBArticulos.put("chocolate", 2.0);
+		dBArticulos.put("agua", 1.0);
+		dBArticulos.put("zumo", 1.67);
+		dBArticulos.put("cereales", 0.56);
+		dBArticulos.put("manzanas", 0.2);
+		dBArticulos.put("aceite", 16.0);
+	}
+	
+	
+	public static void main() {
+		String [] opciones = {"Mostrar stock","Añadir nuevo articulo"};
+		int opcion;
+		boolean exit = false;
+		cargarDB();
+		
+		
+		do {
+			opcion =  AuxMethod.darOpciones(opciones);
+			System.out.println(opcion);
+			switch(opcion) {
+			case 0: // Mostrar venta
+				if (dBArticulos.isEmpty()) {
+					AuxMethod.mostrarInfo("No hay ventas almacenadas.");
+				} else {
+					mostrarStock();
+				}
+				break;
+			case 1: // Añadir venta
+				String nombreArticulo = AuxMethod.solicitarDatosString("Introducir nombre del articulo");
+				double precio =  AuxMethod.solicitarDatosDouble("Introducir cuanto vale el articulo");
+				dBArticulos.put(nombreArticulo, precio);
+				break;
+			default:
+				exit = true;
+				break;
+			}
+			
+		} while (!exit);
+	}
 }
