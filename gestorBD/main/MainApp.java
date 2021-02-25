27090701da52545bcba08767aa51914602864a36
Miguel <sastre113@gmail.com>
2021-02-25 16:26:08 +0100

    prueba

--- gestorBD/main/MainApp.java
+++ gestorBD/main/MainApp.java
@@ -15,6 +15,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo!");
+		System.out.println("Esto es una prueb");
 	}
 
 }
