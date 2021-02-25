b1f3ce902561374130df9cca8c5db78606d6b0be
Miguel <sastre113@gmail.com>
2021-02-25 16:28:23 +0100

    .

--- gestorBD/main/MainApp.java
+++ gestorBD/main/MainApp.java
@@ -15,7 +15,6 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo!");
-		System.out.println("Esto es una prueb");
 	}
 
 }
